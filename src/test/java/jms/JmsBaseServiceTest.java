f97b9b920e86d6f7c74350833255ebe4bbf07f6d
严恺珺 <kaijun@fit2cloud.com>
2018-06-29 18:42:01 +0800

    增加使用token的方式

--- src/test/java/jms/JmsBaseServiceTest.java
+++ src/test/java/jms/JmsBaseServiceTest.java
@@ -13,7 +13,8 @@ public class JmsBaseServiceTest {
 
     @Before
     public void getJmsAssetsService() {
-        jmsBaseService = new JmsBaseService("http://localhost:8088", "admin", "admin");
+//        jmsBaseService = new JmsBaseService("http://localhost:8088", "admin", "admin");
+        jmsBaseService = new JmsBaseService("http://localhost:8088", "b79cfee265082c9f1cc0cec269de8c28c8ee5744");
         String token = jmsBaseService.getToken();
         System.out.println(token);
     }
@@ -23,6 +24,7 @@ public class JmsBaseServiceTest {
         JmsAssetsService jmsAssetsService = jmsBaseService.jmsAssetsService();
         Asset asset = new Asset();
         asset.setHostname("SDK-hostName-modify");
+        asset.setIp("192.168.12.12");
         Map<String, String> map = jmsAssetsService.addAsset(asset);
         System.out.println(map);
     }
